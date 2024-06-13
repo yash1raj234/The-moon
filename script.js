b515e39d429149bd58612de45724ad50a804460f
Yashraj Rai <135339025+yash1raj234@users.noreply.github.com>
2024-06-13 11:49:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+document.getElementById('imageSelector').addEventListener('change', function() {
+
+
+    var selectedImage = this.value;
+    
+    document.getElementById('displayImage').src = selectedImage;
+
+    
+
+
+
+});
+
